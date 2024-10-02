15bdae3b8471fdb8a44c9c0d4de8fc2f089fb13e
vintage254 <derricknjuguna414@gmail.com>
2024-10-02 14:14:56 +0000

    Files manager

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
@@ -7,5 +9,24 @@ router.get('/status', AppController.getStatus);
 
 router.get('/stats', AppController.getStats);
 
+router.post('/users', UsersController.postNew);
+
+router.get('/connect', AuthController.getConnect);
+
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.get('/users/me', UsersController.getMe);
+
+router.post('/files', FilesController.postUpload);
+
+router.get('/files/:id', FilesController.getShow);
+
+router.get('/files', FilesController.getIndex);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
